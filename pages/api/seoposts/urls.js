80a512bbcbfc8dfcf1f674f423b532b762e45f7e
tom813 <tom.engelmann@outlook.de>
2023-03-17 17:42:46 +0100

    netlify fix try

--- pages/api/seoposts/urls.js
+++ pages/api/seoposts/urls.js
@@ -4,7 +4,7 @@ const handler = async (req, res) => {
     try{
         const result = await excuteQuery("SELECT url FROM seo")
 
-        return res.status(200).json(result)
+        return res.json(result)
     } catch(e){
         res.status(500).json({message: e.message})
     }
