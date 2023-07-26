e21a022ef12e77da46a9a2cacfa4b3fbba2817a2
tom813 <tom.engelmann@outlook.de>
2023-07-26 18:15:57 +0200

    update

--- pages/components/ChatbotExample.js
+++ pages/components/ChatbotExample.js
@@ -1,81 +1,13 @@
 import { useState } from "react"
 
 export default function ChatbotExample(props){
-    let chat1 = [
-        {
-            user: true,
-            content: "Wie viele Artikel, die mit der Artikelnummer 123 beginnen sind im System angelegt?",
-            img: ""
-        },
-        {
-            user: false,
-            content: "Aktuell gibt es 6 Artikel, die mit '123' beginnen. Hier ist eine Tabelle mit den Artikeln.",
-            img: ""
-        },
-        {
-            user: false,
-            content: "",
-            img: "/ventil-form2.png"
-        }
-    ]
-    let chat2 = [
-        {
-            user: true,
-            content: "Erstelle ein neues Projekt für die Mustermann GmbH",
-            img: ""
-        },
-        {
-            user: false,
-            content: "Um das Projekt zu erstellen, sind noch folgende Daten notwendig",
-            img: ""
-        },
-        {
-            user: false,
-            content: "",
-            img: "/ventil-table.png"
-        }
-    ]
-    let chat3 = [
-        {
-            user: true,
-            content: "Wie viel Umsatz wurde in den letzten 3 Monaten im Durchschnitt gemacht?",
-            img: ""
-        },
-        {
-            user: false,
-            content: "In den letzten 3 Monaten wurden 546.829,21€ Umsatz gemacht. Hier ist eine Übersicht.",
-            img: ""
-        },
-        {
-            user: false,
-            content: "",
-            img: "/ventil-tabelle-edit.png"
-        }
-    ]
-    let chat4 = [
-        {
-            user: true,
-            content: "Erhöhe den Verkaufspreis für alle Artikel vom Zulieferer Mustermann um 1€",
-            img: ""
-        },
-        {
-            user: false,
-            content: "Hier ist eine Vorschau mit den angepassten Artikeln. Bitte bestätigen Sie die Tabelle.",
-            img: ""
-        },
-        {
-            user: false,
-            content: "",
-            img: "/graph.png"
-        }
-    ]
-
+    
     let [chats, setChat] = useState(props.chats[0]);
     
     return(
         <div className="chatbot-container">
             <h1>Chatbot Beispiele</h1>
-            <div className="chat-container">
+            {/* <div className="chat-container">
                 <div className="chat-examples">
                     {
                         props.chats.map((chat, index) => (
@@ -111,7 +43,7 @@ export default function ChatbotExample(props){
                         </div>
 ))}
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
