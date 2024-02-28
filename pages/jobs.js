6b5d9471dc4f134152cc97c41f0658549ee3fae1
tom813 <tom.engelmann@outlook.de>
2024-02-28 21:10:13 +0100

    change job schema

--- pages/jobs.js
+++ pages/jobs.js
@@ -7,7 +7,7 @@ export default function Jobs() {
     "@context": "https://schema.org/",
     "@type": "JobPosting",
     "title": "Werksstudent(in) Marketing/SEO",
-    "employeeType": "Werksstudent",
+    "employeeType": "OTHER",
     "description": "Die Scalarsoft GmbH in Potsdam sucht einen engagierten Werksstudenten (m/w/d) im Bereich Marketing/SEO...",
     "hiringOrganization": {
       "@type": "Organization",
@@ -47,7 +47,7 @@ export default function Jobs() {
     "@context": "https://schema.org/",
     "@type": "JobPosting",
     "title": "Werksstudent(in) Next.js Entwicklung",
-    "employeeType": "Werksstudent",
+    "employeeType": "OTHER",
     "description": "Die Scalarsoft GmbH in Potsdam sucht einen talentierten Werksstudenten (m/w/d) im Bereich Next.js Entwicklung, der unser Team mit innovativen Webentwicklungsprojekten unterstützt. Deine Aufgaben umfassen die Entwicklung und Optimierung von Webanwendungen mit Next.js, die Verbesserung der Benutzererfahrung und die Implementierung responsiver Designs. Das Gehalt basiert auf deinen Fähigkeiten und Erfahrungen in der Webentwicklung, insbesondere in Next.js, React und modernen Webtechnologien. Diese Position bietet dir die Möglichkeit, in einem dynamischen Umfeld zu arbeiten, deine Fähigkeiten zu erweitern und an spannenden Projekten mitzuarbeiten.",
     "hiringOrganization": {
       "@type": "Organization",
