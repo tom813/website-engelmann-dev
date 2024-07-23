90c180bb06d43d5569bcbb06a86ba1a4ecbdbeac
tom813 <tom.engelmann@outlook.de>
2024-07-23 18:28:51 +0200

    ilb update

--- pages/components/FinancialSupport.js
+++ pages/components/FinancialSupport.js
@@ -3,26 +3,17 @@ export default function FinancialSupport(){
         <div className="financial-support-container">
             <div id="scroll-point-4" />
             <div className="financial-support-info-container">
-                <div className="coding-text">
-                    {"< jetzt_digital >"}
-                </div>
+                
                 <h2>
-                    Föderung mit jetzt digital beantragen
+                    Gefördert durch die EU und das Land Brandenburg
                 </h2>
                 <p>
-                    Der Bund fördert Vorhaben zur Digitalisierung relativ mit bis  zu 60% und absolut mit bis zu 50.000€.
-                    Unsere Softwarelösungen sind förderfähig und wir helfen Ihnen dabei die Zuschüsse zu Ihrem Projekt zu beantragen.
-                    Die Förderung läuft bis Ende 2023, weiter Informationen finden Sie auf der Website des BMWK.
+                    <strong>Vorhaben</strong>: Beschäftigung eines Werksstudenten im Rahmen eines betrieblichen Innovationsprojektes <br />
+                    <strong>Ziel</strong>: Entwicklung eines Chatbots zur Interaktion mit bestehenden Softwaresystemen und Datenbanken.
                 </p>
-                <a target="_blank" rel="noreferrer" href="https://www.bmwk.de/Redaktion/DE/FAQ/Digital-Jetzt/faq-digital-jetzt.html">
-                <button className="secondary-btn">
-                   Zum BMWK
-                </button>
-                </a>
-                
             </div>
             <div className="financial-support-image-container">
-                <img src="/jetzt-digital.png" />
+                <img src="/ilb.png" />
             </div>
         </div>
     )
